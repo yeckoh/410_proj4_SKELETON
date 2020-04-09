f9c62387bae28e916b9c594451da0aada1ace256
wchang <yeckoh@gmail.com>
2020-04-08 18:11:54 -0700

    bake_and_box

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -20,14 +20,14 @@ void Baker::bake_and_box(ORDER &anOrder) {
 	Box box_of_donuts;
 	DONUT fresh_donut;
 	fresh_donut.myType = GLAZED;
-
-	for(int i = anOrder.number_donuts; i > 0; --i) {
-		if (!box_of_donuts.addDonut(fresh_donut)) // missed opportunity to use baker's dozens
-			anOrder.boxes.push_back(box_of_donuts);
-		box_of_donuts.clear();
-	}
-	// package last nonfull. Can be empty like this, oof!
 	anOrder.boxes.push_back(box_of_donuts);
+	int donuts_left = anOrder.number_donuts;
+	while(donuts_left > 0) {
+		if(anOrder.boxes.back().addDonut(fresh_donut))
+			--donuts_left;
+		if(anOrder.boxes.back().size() == 12)
+			anOrder.boxes.push_back(box_of_donuts);
+	}
 }
 
 //as long as there are orders in order_in_Q then
@@ -41,24 +41,24 @@ void Baker::bake_and_box(ORDER &anOrder) {
 //when either order_in_Q.size() > 0 or b_WaiterIsFinished == true
 //hint: wait for something to be in order_in_Q or b_WaiterIsFinished == true
 void Baker::beBaker() {
-	while(!b_WaiterIsFinished && !order_in_Q.empty()) {
+	do {
 		unique_lock<mutex> ticket_rack_is_full(mutex_order_inQ);
+
 		// pretend to work until the waiters put 5million tickets in at once
 		// then panic and tell the front the pos_sys is dead now
-		while (!b_WaiterIsFinished || order_in_Q.empty())
+		while (!b_WaiterIsFinished && order_in_Q.empty())
 			cv_order_inQ.wait(ticket_rack_is_full);
 
 		ORDER current_order;
 		while (!order_in_Q.empty()) {
 			lock_guard<mutex> working_on_this_ticket(mutex_order_outQ); // "don't mess me up, i got this ticket"
-			// ticket_rack_is_full.lock();
 			current_order = order_in_Q.front();
 			order_in_Q.pop(); // wewlad no data is returned, 2 operations for this
-			// ticket_rack_is_full.unlock(); // let waiters put more orders in
 			bake_and_box(current_order);
 			order_out_Vector.push_back(current_order);
 		}
 	}
+	while (!b_WaiterIsFinished && !order_in_Q.empty());
 }
 
 
